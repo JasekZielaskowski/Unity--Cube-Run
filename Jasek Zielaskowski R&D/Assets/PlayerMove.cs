9c6c265b0650dbdc4d7fd4f3cdff899a9673867a
Jasek <02jasek.zielaskowski@gulllakecs.org>
2020-02-21 08:08:27 -0500

    Initial Commit

---
+++ Jasek Zielaskowski R&D/Assets/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+
+    // We are using the physics system
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        {
+
+        }
+    }
+}
