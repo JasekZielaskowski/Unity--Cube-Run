1d0ef5fe63d140dd0795a99bcbe944f2554bcd79
Jasek <02jasek.zielaskowski@gulllakecs.org>
2020-02-21 08:34:02 -0500

    Add player input for "a&d" keys

--- Jasek Zielaskowski R&D/Assets/PlayerMove.cs
+++ Jasek Zielaskowski R&D/Assets/PlayerMove.cs
@@ -5,15 +5,23 @@ public class PlayerMove : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardForce = 2000f;
-
+    public float sidewaysForce = 500f;
     // We are using the physics system
     // Update is called once per frame
     void FixedUpdate()
     {
         // Add a forward force
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        {
 
+        if ( Input.GetKey("d") )
+
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime , 0, 0);
+        }
+        if (Input.GetKey("a"))
+
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
         }
     }
 }
